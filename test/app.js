e00af555975981b2b2bb1f0cb75475fc76d34ed6
robinnag <nagrobin1@gmail.com>
2017-05-11 13:24:08 -0400

    Heroku

--- test/app.js
+++ test/app.js
@@ -6,15 +6,13 @@ module.exports = function(app)
 
     var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-    if(process.env.MLAB_USERNAME) {
-        connectionString = process.env.MLAB_USERNAME + ":" +
-            process.env.MLAB_PASSWORD + "@" +
+    if(process.env.MLAB_USERNAME_WEBDEV) {
+        connectionString = process.env.MLAB_PASSWORD_WEBDEV + ":" +
+            process.env.MLAB_USERNAME_WEBDEV + "@" +
             process.env.MLAB_HOST + ':' +
             process.env.MLAB_PORT + '/' +
             process.env.MLAB_APP_NAME;
     }
-
-
 
 
     var mongoose = require("mongoose");
